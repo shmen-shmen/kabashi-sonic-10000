6c8da953f8e40f2062195f45aa5dbbe8cf5cfa9d
shmen-shmen <akshevchenko@proton.me>
2023-06-06 19:03:34 +0600

    change readme, cleanup

--- src/App.js
+++ src/App.js
@@ -4,16 +4,10 @@ import Filter from "./componets/Filter";
 import Keyboard from "./componets/Keyboard";
 import Envelope from "./componets/Envelope";
 import Delay from "./componets/Delay";
-import { useSelector } from "react-redux";
 
 function App() {
-	const { canSee } = useSelector((state) => state.sliders);
 	return (
-		<div
-			className="App"
-			id="app"
-			// style={canSee ? { color: "white" } : { color: "black" }}
-		>
+		<div className="App" id="app">
 			<h1>kabashi sonic 10000</h1>
 			<div className="synth">
 				<div className="modules">
